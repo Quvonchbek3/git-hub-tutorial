64dd191635aa6eccb7c8106bc4e31303c4a51bde
Quvonchbek <example@gmail.com>
2023-08-21 22:22:15 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,170 @@
+// let number = 10;
+// const borderColor = "red";
+
+// const person = {
+//     name:"Quvonchbek",
+// };
+
+// person.name = "Samar";
+// console.log(person);
+
+// //Old versions
+
+// console.log(clientName);
+
+// var clientName = "Quvonchbek";
+
+
+
+
+//M: Qat'iy rejim
+
+
+// "use strict";
+
+// const number = 10;
+// const clienName = "Quvonchbek";
+// console.log(number);
+
+
+
+
+
+// // M: Ma'lumot turlari
+
+
+// let number = 5.6;   // Data Type = Number
+
+// // Number
+// console.log(-8 / 0);  // Infinitiy
+// console.log("Quvonchbek" * 8);  //NaN
+
+// // String
+// const clientName = `Quvonchbek`
+
+
+// // Boolean
+
+// const isMarried = false;
+// const isCircleEafth = true;
+
+// // true = Ha | to`g`ri
+// // false = Yoq | noto`g`ri
+
+// console.log(clientAge);
+
+// // null
+// console.log(clientAge);
+
+// // undefined 
+
+// let und;
+// console.log(und);
+
+// // object
+
+// const thief = {
+// // key value
+//     age: 30, // number
+//     jacket: "black",  // string
+//     islong: false,  //boolean
+// };
+
+// console.log(thief.age);
+
+// // array
+// //                0       1        2     3    4   5
+// const colors = ["red", "black", "green", 10, [], {}];
+// console.log(colors[0]);
+
+
+
+
+
+// # Foydalanuvchi bilan muloqot qilish.
+
+// "use strict"
+
+// alert ("Hello Javascript");
+
+// const place = confirm("Are you here ?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you", "Quvonchbek");
+// console.log(typeof currentPlace);
+
+
+// const age = +prompt("How old are you ?", "16");
+// console.log(age + 10);  //"16" + 10 = 26
+
+// const favoriteColor =[];
+
+// favoriteColor[0] = prompt("What's your favorite color #1", "");
+// favoriteColor[1] = prompt("What's your favorite color #2", "");
+// favoriteColor[2] = prompt("What's your favorite color #3", "");
+// console.log(favoriteColor);    // type of qilsak object keladi
+
+
+
+
+
+// # Interpolatsiya
+
+
+// "use strict"
+
+// const channel = prompt("What's your favorite youtuber");
+
+
+// console.log("https://youtube.com/" + channel +"/videos");  oddiy usul;
+// console.log(`https://youtube.com/${channel}`);   interpolatsiya usul;
+
+
+
+// const user = "Samar";
+// const job = "JavaScript developer";
+// console.log(`User name is ${user}, he is ${job}`);
+
+// # Operatorlar
+
+
+"use strict";
+
+
+// console.log(5 + "9");
+
+
+// incr, decr
+
+
+// let incr = 10;
+// let decr = 10;
+
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+
+
+
+// console.log(5 % 2);
+
+
+// console.log(5 * 5 === "25");
+
+
+
+// && = va  || = yoki 
+
+// const isAge = true;
+// const isClose = false;
+
+// console.log(isAge && isClose);
+
+
+
+// console.log(2 + 2 * 2 !== 6);
+
+
